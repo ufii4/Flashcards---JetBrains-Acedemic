9936670e020262454f507f9ce6c1e01b43d032dc
Alex Xie <pc.xie00@gmail.com>
2020-07-06 22:40:53 -0400

    Version 1.0
    Should be fine to operate except some small bugs.

--- src/flashcards/Main.java
+++ src/flashcards/Main.java
@@ -1,5 +1,8 @@
 package flashcards;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
 
 public class Main {
@@ -8,27 +11,26 @@ public class Main {
         startMenu();
     }
 
-    public String getKey(Map<String, String> map, String val) {
-        for (String key : map.keySet()) {
-            if (map.get(key).equals(val)) {
-                return key;
-            }
-        }
-        return null;
-    }
+    public static int numberOfCards;
 
     public static void startMenu() {
         Scanner sc = new Scanner(System.in);
         Map<String, String> flashCards = new HashMap<>();
         while (true) {
             System.out.println("Input the action (add, remove, import, export, ask, exit):");
-            String action = sc.nextLine().toLowerCase();
+            String action = sc.nextLine();
             if (action.equals("add")) {
                 addCard(flashCards, sc);
             } else if (action.equals("remove")) {
                 removeCard(flashCards, sc);
             } else if (action.equals("exit")) {
                 break;
+            } else if (action.equals("import")) {
+                importFromFile(flashCards, sc);
+            } else if (action.equals("export")) {
+                exportToFile(flashCards, sc);
+            } else if (action.equals("ask")) {
+                askQuestions(flashCards, sc);
             } else {
                 System.out.println("Please enter a valid action.");
             }
@@ -52,6 +54,7 @@ public class Main {
             return;
         }
         flashCards.put(description, definition);
+        numberOfCards++;
         System.out.println("The pair (\"" + description + "\":\"" + definition + "\") has been added.");
         return;
     }
@@ -59,9 +62,10 @@ public class Main {
     public static void removeCard(Map<String, String> flashCards, Scanner sc) {
         String description;
         System.out.println("The card:");
-        description = sc.nextLine().toLowerCase();
+        description = sc.nextLine();
         if (flashCards.containsKey(description)) {
             flashCards.remove(description);
+            numberOfCards--;
             System.out.println("The card has been removed.");
         } else {
             System.out.println("Can't remove \"" + description + "\": there is no such card.");
@@ -70,14 +74,83 @@ public class Main {
     }
 
     public static void importFromFile(Map<String, String> flashCards, Scanner sc) {
-
+        System.out.println("File name:");
+        File myCards = new File(sc.nextLine());
+        int sum = 0;
+        try (Scanner cardReader = new Scanner(myCards)) {
+            if (cardReader.nextLine().equals("This file is for the project flashcards.")) {
+                String[] input;
+                String description;
+                String definition;
+                while (cardReader.hasNextLine()) {
+                    input = cardReader.nextLine().split(":");
+                    description = input[0];
+                    definition = input[1];
+                    if (flashCards.containsKey(description) || flashCards.containsValue(definition)) {
+                        numberOfCards--;
+                    }
+                    flashCards.put(description, definition);
+                    sum++;
+                }
+                numberOfCards += sum;
+                System.out.println(sum + " cards have been loaded.");
+                return;
+            }
+        } catch (IOException e) {
+        }
+        System.out.println("0 cards loaded. File not found or file format not supported");
+        return;
     }
 
     public static void exportToFile(Map<String, String> flashCards, Scanner sc) {
-
+        System.out.println("File name:");
+        //If the file already exist, ask if to rewrite it.
+        File myCards = new File(sc.nextLine());
+        int sum = 0;
+        try (PrintWriter cardsWriter = new PrintWriter(myCards)) {
+            cardsWriter.println("This file is for the project flashcards.");
+            for (String description : flashCards.keySet()) {
+                sum++;
+                cardsWriter.println(description + ':' + flashCards.get(description));
+            }
+            System.out.println(sum + (sum == 1 ? " card" : " cards") + " have been saved.");
+        } catch (IOException e) {
+            System.out.printf("An exception occurs %s", e.getMessage());
+        }
+        return;
     }
 
     public static void askQuestions(Map<String, String> flashCards, Scanner sc) {
+        System.out.println("How many times to ask?");
+        int turns = Integer.parseInt(sc.nextLine());
+        String answer;
+        String nextCard;
+        Random index = new Random();
+        for (int i = 0; i < turns; i++) {
+            nextCard = (String) flashCards.keySet().toArray()[index.nextInt(numberOfCards)];
+            System.out.println("Print the definition of \"" + nextCard + "\":");
+            answer = sc.nextLine();
+            if (flashCards.get(nextCard).equals(answer)) {
+                System.out.println("Correct answer.");
+            } else {
+                System.out.print("Wrong answer. The correct one is \"" + flashCards.get(nextCard) + "\"");
+                if (flashCards.containsValue(answer)) {
+                    System.out.print(", you've just written the definition of \"" + getKeyFromValue(flashCards, answer) + "\"");
+                }
+                System.out.println(".");
+            }
+            if (i >= turns) {
+                break;
+            }
+        }
+    }
 
+    public static String getKeyFromValue(Map<String, String> map, String val) {
+        for (String key : map.keySet()) {
+            if (map.get(key).equals(val)) {
+                return key;
+            }
+        }
+        return null;
     }
 }
